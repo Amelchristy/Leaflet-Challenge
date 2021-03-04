a4ad0247bf2384821a38773c660fa42061646e47
Amelchristy <Amel_christy@users.noreply.github.com>
2021-03-04 16:39:21 -0500

    Push

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = 
+const API_KEY = "pk.eyJ1IjoiY2VtZW5nOTciLCJhIjoiY2tsdmRxdTM1MDFrMjJ1a3ZhdWQxOWl6cSJ9.OEVqKo_MG04sf8rkkyevUA";
--- Leaflet-Step-2/static/js/config.js
+++ Leaflet-Step-2/static/js/config.js
@@ -1,3 +1,3 @@
 // API key
-const API_KEY = "";
+const API_KEY = "pk.eyJ1IjoiY2VtZW5nOTciLCJhIjoiY2tsdmRxdTM1MDFrMjJ1a3ZhdWQxOWl6cSJ9.OEVqKo_MG04sf8rkkyevUA";
 
